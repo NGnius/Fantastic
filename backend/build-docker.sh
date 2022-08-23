cac38d9809399fa11f1592ea67d2ba453e43f073
TrainDoctor <traindoctor@protonmail.com>
2022-08-23 14:39:10 -0700

    Remove sudo from this line

--- backend/build-docker.sh
+++ backend/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo build --release
+$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo build --release
 mkdir -p out
 cp target/release/fantastic-rs out/backend
