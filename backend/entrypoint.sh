bed6b096be61c5d7c9839d5d1fb744421e35b840
TrainDoctor <traindoctor@protonmail.com>
2022-08-23 14:04:18 -0700

    rustup is installed to root, grab it from there

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,6 @@ echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
 cd /backend
 
-$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo build --release
+/root/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo build --release
 mkdir -p out
 cp target/release/fantastic-rs out/backend
