aaf6802e6936e7675b7e8eaa76c4a3d73bc6ad09
TrainDoctor <traindoctor@protonmail.com>
2022-08-23 14:26:05 -0700

    Added separate build-docker.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,4 @@ echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
 cd /backend
 
-/root/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo build --release
-mkdir -p out
-cp target/release/fantastic-rs out/backend
+sudo bash build-docker.sh
