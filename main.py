95dab8017113642bc221b2435f65da52716f42b5
NGnius (Graham) <ngniusness@gmail.com>
2023-02-23 21:15:33 -0500

    Update libs and frameworks

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ class Plugin:
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         # startup
-        self.backend_proc = subprocess.Popen([PARENT_DIR + "/bin/backend"])
+        self.backend_proc = subprocess.Popen(
+            [PARENT_DIR + "/bin/backend"],
+            env = dict(os.environ))
         while True:
             await asyncio.sleep(1)
+
+    async def _unload(self):
+        # shutdown
+        if self.backend_proc is not None:
+            self.backend_proc.terminate()
+            try:
+                self.backend_proc.wait(timeout=5) # 5 seconds timeout
+            except subprocess.TimeoutExpired:
+                self.backend_proc.kill()
+            self.backend_proc = None
